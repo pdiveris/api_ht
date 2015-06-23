f8e9dbb5571fc97d3eece34450c8dc953f2d2e84
Petros Diveris <petros@diveris.org>
2015-06-23 13:42:32 +0100

    Refactored JorumModel to JiscModel. Moved Formatters up (so they can be used by all APIs. Fixed Contect dependencies

--- src/Jisc/api/Service/Context.php
+++ src/Jisc/api/Service/Context.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * Jorum API Context
+ * Jisc API Context
  *
  * Context
  *
  * Provides information about the context of a request that results in a list
  * such as page number, item count etc.
  *
- * @package      MIMAS
+ * @package      API
  * @subpackage   Service
  * @category     API
- * @version      0.9.0
+ * @version      1.0
  * @author       Petros Diveris <petros.diveris@manchester.ac.uk>
  */
-namespace MIMAS\Service;
+namespace Jisc\api\Service;
 
 /**
  * Created by PhpStorm.
@@ -21,151 +21,151 @@ namespace MIMAS\Service;
  * Date: 25/04/2014
  * Time: 14:59
  */
-class Context extends JorumModel
+class Context extends JiscModel
 {
-    /**
-     * limit
-     * @var int $limit
-     */
-    protected $limit = 0;
+  /**
+   * limit
+   * @var int $limit
+   */
+  protected $limit = 0;
 
-    /**
-     * offset
-     * @var int $offset
-     */
-    protected $offset = 0;
+  /**
+   * offset
+   * @var int $offset
+   */
+  protected $offset = 0;
 
-    /**
-     * totalCount
-     * @var int $totalCount
-     */
-    protected $totalCount = 0;
+  /**
+   * totalCount
+   * @var int $totalCount
+   */
+  protected $totalCount = 0;
 
-    /**
-     * query
-     * @var string $query
-     */
-    protected $query = '';
+  /**
+   * query
+   * @var string $query
+   */
+  protected $query = '';
 
-    /**
-     * error
-     * @var string $error
-     */
-    protected $error = '';
+  /**
+   * error
+   * @var string $error
+   */
+  protected $error = '';
 
-    /**
-     * queryDate
-     * @var string $queryDate
-     */
-    protected $queryDate = '';
+  /**
+   * queryDate
+   * @var string $queryDate
+   */
+  protected $queryDate = '';
 
-    /**
-     * Constructor. Data or format passed to Base model
-     *
-     * Constructor. Set the properties based on data array passed, if any
-     * @param null $data
-     */
-    public function __construct($data = null)
-    {
-        if (is_array($data)) {
-            foreach ($data as $key => $value) {
-                $key = camel_case($key);
-                if (property_exists($this, $key)) {
-                    $this->$key = $value;
+  /**
+   * Constructor. Data or format passed to Base model
+   *
+   * Constructor. Set the properties based on data array passed, if any
+   * @param null $data
+   */
+  public function __construct($data = null)
+  {
+    if (is_array($data)) {
+      foreach ($data as $key => $value) {
+        $key = camel_case($key);
+        if (property_exists($this, $key)) {
+          $this->$key = $value;
 
-                }
-            }
         }
+      }
     }
+  }
 
-    /**
-     * Get limit
-     * @return int limit
-     */
-    public function getLimit()
-    {
-        return $this->limit;
-    }
+  /**
+   * Get limit
+   * @return int limit
+   */
+  public function getLimit()
+  {
+    return $this->limit;
+  }
 
-    /**
-     * Set limit
-     * @param $limit
-     */
-    public function setLimit($limit)
-    {
-        $this->limit = $limit;
-    }
+  /**
+   * Set limit
+   * @param $limit
+   */
+  public function setLimit($limit)
+  {
+    $this->limit = $limit;
+  }
 
-    /**
-     * Get offset
-     * @return int offset
-     */
-    public function getOffset()
-    {
-        return $this->offset;
-    }
+  /**
+   * Get offset
+   * @return int offset
+   */
+  public function getOffset()
+  {
+    return $this->offset;
+  }
 
-    /**
-     * Set offset
-     * @param $offset
-     */
-    public function setOffset($offset)
-    {
-        $this->offset = $offset;
-    }
+  /**
+   * Set offset
+   * @param $offset
+   */
+  public function setOffset($offset)
+  {
+    $this->offset = $offset;
+  }
 
-    /**
-     * Get totalCount
-     * @return int totalCount
-     */
-    public function getTotalCount()
-    {
-        return $this->totalCount;
-    }
+  /**
+   * Get totalCount
+   * @return int totalCount
+   */
+  public function getTotalCount()
+  {
+    return $this->totalCount;
+  }
 
-    /**
-     * Set totalCount
-     * @param $totalCount
-     */
-    public function setTotalCount($totalCount)
-    {
-        $this->totalCount = $totalCount;
-    }
+  /**
+   * Set totalCount
+   * @param $totalCount
+   */
+  public function setTotalCount($totalCount)
+  {
+    $this->totalCount = $totalCount;
+  }
 
-    /**
-     * Set query
-     * @return string query
-     */
-    public function getQuery()
-    {
-        return $this->query;
-    }
+  /**
+   * Set query
+   * @return string query
+   */
+  public function getQuery()
+  {
+    return $this->query;
+  }
 
-    /**
-     * Set query
-     * @param $query
-     */
-    public function setQuery($query)
-    {
-        $this->query = $query;
-    }
+  /**
+   * Set query
+   * @param $query
+   */
+  public function setQuery($query)
+  {
+    $this->query = $query;
+  }
 
-    /**
-     * Get Error
-     * @return string error
-     */
-    public function getError()
-    {
-        return $this->error;
-    }
+  /**
+   * Get Error
+   * @return string error
+   */
+  public function getError()
+  {
+    return $this->error;
+  }
 
-    /**
-     * Set error
-     * @param $error
-     */
-    public function setError($error)
-    {
-        $this->error = $error;
-    }
+  /**
+   * Set error
+   * @param $error
+   */
+  public function setError($error)
+  {
+    $this->error = $error;
+  }
 
 }
